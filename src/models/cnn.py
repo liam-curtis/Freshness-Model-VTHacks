b8e4b78b33ae85b67f534bdda6d1a8b105097b72
David Degaraff <123874938+davidjdeg2@users.noreply.github.com>
2023-09-16 14:19:46 -0400

    Update cnn.py

--- src/models/cnn.py
+++ src/models/cnn.py
@@ -1,54 +1,12 @@
-import sys
-sys.path.append('srv/freshnessmodel/src')
-import utils
-                
 import torch
 from torch import nn
-from torch.utils.data import DataLoader
-from torchvision import datasets
-from torchvision.transforms import ToTensor, Lambda, Compose
-import matplotlib.pyplot as plt
-
-
-# Download training data from open datasets.
-training_data = 
-
-# Download test data from open datasets.
-test_data = 
-
-batch_size = 64
-
-# Create data loaders.
-train_dataloader = DataLoader(training_data, batch_size=batch_size)
-test_dataloader = DataLoader(test_data, batch_size=batch_size)
-
-for X, y in test_dataloader:
-    print("Shape of X [N, C, H, W]: ", X.shape)
-    print("Shape of y: ", y.shape, y.dtype)
-    break
-
-# Display sample data
-figure = plt.figure(figsize=(10, 8))
-cols, rows = 5, 5
-for i in range(1, cols * rows + 1):
-    idx = torch.randint(len(test_data), size=(1,)).item()
-    img, label = test_data[idx]
-    figure.add_subplot(rows, cols, i)
-    plt.title(label)
-    plt.axis("off")
-    plt.imshow(img.squeeze(), cmap="gray")
-plt.show()
-
-# Get cpu or gpu device for training.
-device = "cuda" if torch.cuda.is_available() else "cpu"
-print("Using {} device".format(device))
 
 # Creates a class for the convolutional neural network inheriting from the pytorch library
 
 # Define model - takes input as 144*144*3
-class NeuralNetwork(nn.Module):
+class CNNModel(nn.Module):
     def __init__(self):
-        super(NeuralNetwork, self).__init__()
+        super(CNNModel, self).__init__()
         self.network = nn.Sequential(
             
             nn.Conv2d(3, 32, kernel_size = 3, padding = 1),
@@ -82,20 +40,7 @@ class NeuralNetwork(nn.Module):
         logits = self.linear_relu_stack(x)
         return logits
 
-model = NeuralNetwork().to(device)
-print(model)
-
-
-# Calculate cross-entropy loss and optimize model
-
-
-loss_fn = nn.CrossEntropyLoss()
-learning_rate = 1e-3
-optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
-
-
 # Makes predictions on data and uses prediction error to modify model parameters
-
 
 def train(dataloader, model, loss_fn, optimizer):
     size = len(dataloader.dataset)
@@ -115,9 +60,7 @@ def train(dataloader, model, loss_fn, optimizer):
             loss, current = loss.item(), batch * len(X)
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
-
 # Check model performance against test dataset
-
 
 def test(dataloader, model):
     size = len(dataloader.dataset)
@@ -132,53 +75,3 @@ def test(dataloader, model):
     test_loss /= size
     correct /= size
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
-
-
-# Runs above functions to train the model in a specified number of epochs
-
-
-epochs = 15
-for t in range(epochs):
-    print(f"Epoch {t+1}\n-------------------------------")
-    train(train_dataloader, model, loss_fn, optimizer)
-    test(test_dataloader, model)
-print("Done!")
-
-
-# Saves model by serializing to an internal state dictionary
-
-
-torch.save(model.state_dict(), "/srv/freshnessmodel")
-print("Saved PyTorch Model State to model.pth")
-
-
-# Loads model from saved dictionary
-
-model = NeuralNetwork()
-model.load_state_dict(torch.load("/srv/freshnessmodel"))
-
-
-# Initialize possible classes corresponding to redp
-
-
-classes = [
-    "T-shirt/top",
-    "Trouser",
-    "Pullover",
-    "Dress",
-    "Coat",
-    "Sandal",
-    "Shirt",
-    "Sneaker",
-    "Bag",
-    "Ankle boot",
-]
-
-# Make predictions with the model
-
-model.eval()
-x, y = test_data[0][0], test_data[0][1]
-with torch.no_grad():
-    pred = model(x)
-    predicted, actual = classes[pred[0].argmax(0)], classes[y]
-    print(f'Predicted: "{predicted}", Actual: "{actual}"')
