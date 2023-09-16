ff826a678fc0bba74054b460df9d057eda0121ce
branboy <111902862+branboy@users.noreply.github.com>
2023-09-16 18:14:40 -0400

    Add files via upload
    Skeleton for the front end website

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
+from werkzeug.utils import secure_filename
+import os
+from wtforms.validators import InputRequired
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'supersecretkey'
+app.config['UPLOAD_FOLDER'] = 'testing'
+
+class UploadFileForm(FlaskForm):
+    file = FileField("File", validators=[InputRequired()])
+    submit = SubmitField("Upload File")
+
+@app.route('/', methods=['GET',"POST"])
+@app.route('/home', methods=['GET',"POST"])
+def home():
+    form = UploadFileForm()
+    if form.validate_on_submit():
+        file = form.file.data # First grab the file
+        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
+        return render_template('success.html')
+    return render_template('index.html', form=form)
+@app.route('/login')
+def login():
+    return render_template('login.html')
+@app.route('/about_us')
+def about_us():
+    return render_template('about.html')
+@app.route('/success')
+def success():
+    return render_template('success.html')
+@app.route('/register')
+def register():
+    return  render_template('register.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
