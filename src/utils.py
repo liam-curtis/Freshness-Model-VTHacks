7219840df383a4cf8ca6d5250fa4617622f171f8
David Degaraff <123874938+davidjdeg2@users.noreply.github.com>
2023-09-16 23:27:02 -0400

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -77,7 +77,8 @@ def load_images_as_tensors(directory, base_path="."):
 
     for category in categories:
         # Construct the path to the category directory
-        category_dir = os.path.join(base_path, directory, category)
+        category_dir = os.path.join(base_path, str(directory), category)
+
     
         for image_name in os.listdir(category_dir):
             image_path = os.path.join(category_dir, image_name)
