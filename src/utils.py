092583aac5865f37657e8799c3608467d3b10b68
David Degaraff <123874938+davidjdeg2@users.noreply.github.com>
2023-09-17 02:09:38 -0400

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -77,7 +77,7 @@ def get_loss_fn():
     return nn.CrossEntropyLoss()
 
 
-def get_optimizer(model, learning_rate=2e-3):
+def get_optimizer(model, learning_rate=5e4):
     return torch.optim.SGD(model.parameters(), lr=learning_rate)
 
 from torch.utils.data import Dataset
@@ -132,7 +132,7 @@ def load_images_as_tensors(directory, base_path="."):
             # Append the tensor to the appropriate list
             if "rotten" in category:
                 data['rotten'].append(tensor_image)
-            else:
+            else if "fresh" in category:
                 data['fresh'].append(tensor_image)
 
     return data
@@ -159,7 +159,7 @@ def train_and_evaluate_cnn():
     print(model)
 
     loss_fn = get_loss_fn()
-    learning_rate = 2e-3
+    learning_rate = 5e-4
     optimizer = get_optimizer(model, learning_rate)
 
     epochs = 7
