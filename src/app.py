ca9b2054da324f2d47ab6c5fee67c5f14db6129e
David Degaraff <123874938+davidjdeg2@users.noreply.github.com>
2023-09-16 21:00:15 -0400

    Update app.py

--- src/app.py
+++ src/app.py
@@ -2,6 +2,6 @@ import sys
 from utils import train_and_evaluate_cnn
 
 
-model = utils.train_and_evaluate_cnn()
+model = train_and_evaluate_cnn()
 print("Model trained and evaluated successfully!")
 
