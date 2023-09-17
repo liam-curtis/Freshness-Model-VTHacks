01f34de7bf49e8314fd99030e42a008ccfb23629
veljkocvetkovic <veljko.cvetkovich@gmail.com>
2023-09-16 22:19:12 -0400

    added database functionality

--- src/flask_template.py
+++ src/flask_template.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, flash
 from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField
 from werkzeug.utils import secure_filename
@@ -15,38 +15,13 @@ app = Flask(__name__, template_folder='templateFiles', static_folder='staticFile
 app.config['SECRET_KEY'] = 'supersecretkey'
 app.config['UPLOAD_FOLDER'] = 'testing'
 
-class UploadFileForm(FlaskForm):
-    file = FileField("File", validators=[InputRequired()])
-    submit = SubmitField("Upload File")
-
-@app.route('/', methods=['GET',"POST"])
-@app.route('/home', methods=['GET',"POST"])
-def home():
-    form = UploadFileForm()
-    if form.validate_on_submit():
-        file = form.file.data # First grab the file
-        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
-        return render_template('success.html')
-    return render_template('index.html', form=form)
-@app.route('/login')
-def login():
-    return render_template('login.html')
-@app.route('/about_us')
-def about_us():
-    return render_template('about.html')
-@app.route('/success')
-def success():
-    return render_template('success.html')
-@app.route('/register')
-def register():
-    return  render_template('register.html')
-
 # @app.route('/')
 # def welcome():
 #     return "This is the home page of Flask Application"
  
 # Configure the database connection URI
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://davidjdeg2:Snowden37606!@70.161.68.15/Website_DB'
+#db_password = os.environ.get('DATABASE_PASSWORD')
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://davidjdeg2:Snowden37606!@70.161.68.15/Website_DB'
 
 # # Create a SQLAlchemy instance
 db = SQLAlchemy(app)
@@ -66,8 +41,97 @@ user_data = [{'id': result[0], 'username': result[1], 'email': result[2]} for re
 print(user_data)
 
 # #Close the curso
-cursor.close()
 
+# @app.route('/register', methods=['GET', 'POST'])
+# def registerUser():
+#     if request.method == 'POST':
+#         username = request.form['user']
+#         password = request.form['user_pass']
+#         email = request.form['mail']
+#         first_name = request.form['first_name']
+#         last_name = request.form['last_name']
+
+#     return render_template('login.html')
+
+class UploadFileForm(FlaskForm):
+    file = FileField("File", validators=[InputRequired()])
+    submit = SubmitField("Upload File")
+
+@app.route('/', methods=['GET',"POST"])
+@app.route('/home', methods=['GET',"POST"])
+def home():
+    return render_template('index.html')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['user']
+        password = request.form['user_pass']
+
+        unique_query = text('CALL VerifyUsernamePassword(:username, :password, @result);')
+        db.session.execute(unique_query, {'username': username, 'password': password})
+        result = db.session.execute(text('SELECT @result')).scalar()
+        print(result)
+
+        # Access the value of the output parameter directly
+        #unique_result[0]=1
+        if result == 1:
+            # Call the stored procedure to insert the user data into the database
+            return redirect(url_for('upload'))  # Redirect to the login page after registration
+        else:
+            flash('Username or password is incorrect', 'danger')
+
+    return render_template('login.html')
+@app.route('/upload', methods=['GET',"POST"])
+def upload():
+    form = UploadFileForm()
+    if form.validate_on_submit():
+        file = form.file.data # First grab the file
+        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
+        return render_template('success.html')
+    return render_template('upload.html', form=form)
+@app.route('/about_us')
+def about_us():
+    return render_template('about.html')
+@app.route('/success')
+def success():
+    return render_template('success.html')
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['user']
+        password = request.form['user_pass']
+        email = request.form['mail']
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+
+        unique_query = text('CALL VerifyUniqueNameEmail(:username, :email, @result);')
+        db.session.execute(unique_query, {'username': username, 'email': email})
+        result = db.session.execute(text('SELECT @result')).scalar()
+        print(result)
+
+        # Access the value of the output parameter directly
+        #unique_result[0]=1
+        if result == 1:
+            # Call the stored procedure to insert the user data into the database
+            insert_query = text('CALL InsertUserWithHashedPassword(:username, :password, :email, :first_name, :last_name);')
+            insert_values = {
+                'username': username,
+                'password': password,
+                'email': email,
+                'first_name': first_name,
+                'last_name': last_name
+            }
+            db.session.execute(insert_query, insert_values)
+            db.session.commit()  # Commit the transaction
+            flash('Registration successful! You can now log in.', 'success')
+            return redirect(url_for('login'))  # Redirect to the login page after registration
+        else:
+            flash('Username or email is already in use. Please choose another.', 'danger')
+
+    return render_template('register.html')
+
+
+cursor.close()
 @app.route('/')
 def index():
     return render_template('index.html')
