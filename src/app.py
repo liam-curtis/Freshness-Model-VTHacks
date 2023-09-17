373a9486cb632a0b95c123ed9d5dc2fdb0a50552
David Degaraff <123874938+davidjdeg2@users.noreply.github.com>
2023-09-16 20:11:00 -0400

    Update app.py

--- src/app.py
+++ src/app.py
@@ -2,13 +2,8 @@ import sys
 sys.path.append('srv/freshnessmodel/Freshness-Model-VTHacks/src')
 from utils import train_and_evaluate
 
-app = Flask(__name__, template_folder='templateFiles', static_folder='staticFiles')
 
-@app.route('/train_and_evaluate', methods=['GET'])
-def train_and_evaluate():
-    model = utils.train_and_evaluate_cnn()
-    # You can return any relevant information about the model here, for example:
-    return jsonify({"message": "Model trained and evaluated successfully!"})
+model = utils.train_and_evaluate_cnn()
+# You can return any relevant information about the model here, for example:
+return jsonify({"message": "Model trained and evaluated successfully!"})
 
-if __name__ == '__main__':
-    app.run(debug=True)
